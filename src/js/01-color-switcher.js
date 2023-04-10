45920554a3a15e1ae63963256ac46a77f8c75e42
Evgeniy <yaremenko.x@gmail.com>
2023-04-10 21:12:47 +0300

    done hw - 9.3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+}
 
+let timerRandom = null; 
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', () => {
+
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+    timerRandom = setInterval( () => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+})
+
+refs.btnStop.addEventListener('click', () => {
+    clearInterval(timerRandom);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+})
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,64 @@
+import { Notify } from 'notiflix';
+
+Notify.init({
+  width: '300px',
+  distance: '20px',
+  opacity: 0.9,
+  borderRadius: '50px',
+  timeout: 3000,
+  clickToClose: true,
+});
+
+const refs = {
+  form: document.querySelector('form'),
+  firstDalay: document.querySelector('[name="delay"]'),
+  stepDalay: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]')
+}
+
+let counter = 0;
+
+
+refs.form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let DELAY = Number(refs.firstDalay.value);
+  const STEP = Number(refs.stepDalay.value);
+  const amountMessage = Number(refs.form.amount.value);
+  
+    const timerId = setInterval(() => {
+      counter++;
+      if(counter > amountMessage) {
+        clearInterval(timerId);
+        counter = 0;
+        return
+      }
+      startPromise(counter, DELAY); 
+      DELAY += STEP;
+      
+    }, DELAY);
+  
+})
+
+function startPromise (counter, delay) {
+  createPromise(counter, delay)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+  });
 }
+
+
