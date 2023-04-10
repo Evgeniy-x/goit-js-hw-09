2b819dfdc4833ca08dad583ed4a7a26aaf248a86
Evgeniy-x <117513013+Evgeniy-x@users.noreply.github.com>
2023-04-10 18:20:17 +0000

    Deploying to gh-pages from @ Evgeniy-x/goit-js-hw-09@45920554a3a15e1ae63963256ac46a77f8c75e42 🚀

---
+++ 01-color-switcher.a5e66396.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};let e=null;t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(()=>{t.btnStart.disabled=!0,t.btnStop.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.btnStop.addEventListener("click",(()=>{clearInterval(e),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.a5e66396.js.map
